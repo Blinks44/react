6d7c889dd879860d3d1a9d2618ceb16126b24dea
Sergey Vrenev <blinks44@yandex.ru>
2025-04-09 00:50:46 +0300

    wip

--- src/components/product/ProductSingle.jsx
+++ src/components/product/ProductSingle.jsx
@@ -1,5 +1,30 @@
+import { Box, Container, Flex, Grid, Heading, Image, Text } from "@chakra-ui/react";
+
 function ProductSingle({ product }) {
-  return <div>{product.title}</div>;
+  return (
+    <Grid
+      as={"article"}
+      gridTemplateColumns={{ md: "50% 1fr" }}
+      gridTemplateAreas={"image title" + "image description"}
+      gridTemplateRows={"auto auto auto"}
+      alignItems={"start"}
+      gap={4}
+    >
+      <Heading as={"h1"} fontSize={{ md: "xl" }} gridArea={"title"}>
+        {product.title}
+      </Heading>
+      <Image
+        src={product.images[0]}
+        alt={product.title}
+        objectFit="cover"
+        w="full"
+        maxH={"300px"}
+        order={{ md: "-1" }}
+        gridArea={"image"}
+      />
+      <Text gridArea={"description"}>{product.description}</Text>
+    </Grid>
+  );
 }
 
 export default ProductSingle;
